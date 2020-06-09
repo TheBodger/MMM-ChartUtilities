8d4aa3a11d9953de2fdda51b6dec2f6714488dac
N S <neil.scott2@virgin.net>
2020-06-09 11:51:17 +0100

    adding merge

--- common.js
+++ common.js
@@ -296,143 +296,169 @@ exports.mergeutils = function () {
     //
     //
 
+    //TODO handle duplicate records with all keys the same
 
     var _storedsetids = [];
     var _setkeys = {};
-    var _keyset;
+    var _keyset;                //store the first matchkey setid or if blank, the first set passed for storage regardless
     var _path = '';
     var _fs = fs;
     var _presort;
     var _casesensitive;
+    var _matchkeys = [];
+    var _sortkeys = [];
+    var _output = {};
+    var _mergesets = {};
+    var _mergesetindexes = {};
 
-    
+    this.storedsetids = function () {
+        return _storedsetids;
+    }
+
     this.init = function (config) {
 
         //config is the merge part of the overall confg
 
         //
-//				merge {
-//					fileprocess: true/false					// if true will serialise all the data to disk and use file processing instead of in memory prior to building the ouput
-//					input: [setids]							// list of the setids to monitor/store/process
-//					sort: true/false						// use the matchkeys to sort each setid
-//					casesensitive:true/false				//sorting and matching is sensitive/insensitive to case
-//					matchkeys: [[setid.key,setid.key,..]]	// multiple key levels matching from left to right
-//															// key is the AKA name of a field, the first one will provide the setid.key in the output
-//															// the first key is king and everything must match that record by record or get discarded
-//					output: [setid.field,setid.field,setid.field,..] // will produce a [setid.key:{setid.filed,etc,etc}]
-//															// if left blank will copy over any field not present already in the ouput from the setid key/values
-//					
+        //				merge {
+        //					fileprocess: true/false					// if true will serialise all the data to disk and use file processing instead of in memory prior to building the ouput
+        //					input: [setids]							// list of the setids to monitor/store/process
+        //					sort: true/false						// use the matchkeys to sort each setid
+        //					casesensitive:true/false				//sorting and matching is sensitive/insensitive to case
+        //					matchkeys: [setid.key, setid.key,..]	// multiple key levels matching from left to right
+        //															// key is the AKA name of a field, the first one will provide the setid.key in the output
+        //															// the first key is king and everything must match that record by record or get discarded
+        //					output: [setid.field,setid.field,setid.field,..] // will produce a [setid.key:{setid.filed,etc,etc}]
+        //															// if left blank will copy over any field not present already in the ouput from the setid key/values
+        //					
 
-        if (config.fileprocess) {
+        if (!config.fileprocess) {
             _fs = ms;
             _path = '/';
         }
 
         _presort = config.sort;
-
         _casesensitive = config.casesensitive;
+        _matchkeys = config.matchkeys;
 
+        //setup the default sortkey
 
+        if (_matchkeys == null) { _matchkeys.push('subject'); } //add subject as the default key
+        else if (_matchkeys.length == 0) { _matchkeys.push('subject'); } //add subject as the default key
+        else if (_keyset == null) {
+            _keyset = _matchkeys[0].split(".")[0];//get the keyset from the first key in matchkeys setid.key
+            for (var idx = 0; idx < _matchkeys.length; idx++) {
+                _matchkeys[idx] = _matchkeys[idx].split(".")[1]; //remove the setid as we have stored it
+            }
+        }
 
+        return _matchkeys;
     }
 
     this.mergesets = function () {
 
+        var self = this;
+
         //todo - add option to choose the key set and not just use default of the first one
 
-        var _mergesets = {};
         var _mergedsets = {};
-        var _mergekeyvalues = {};
-        var _mergesetindexes = {};
-        var _maxkeyvalue;
         var _processedsetcount = 0;
-        var _output = {};
+
+        var self = this;
 
         //recall all setids stored, use the 1st set to drive, all will be loaded back into set format
         //and preload the current key
 
-        for (var _setid in _storedsetids) {
+        _storedsetids.forEach(function (_setid) {
 
-            _mergesets[_setid] = this.recallset(_setid);
-            _mergekeyvalues[_setid] = _mergesets[_setid][0][_setkeys[_setid]] //should return the setkey of the first entry in a set of array
+            _mergesets[_setid] = self.recallset(_setid);
             _mergesetindexes[_setid] = 0;
 
-        }
-
-        //create a maximum key value to enable processing below
-
-        _maxkeyvalue = _mergesets[_keyset][_mergesets[_keyset].length-1][_setkeys[_keyset]];
-
-        if (typeof (_maxkeyvalue) == 'number') {
-            _maxkeyvalue++;
-        } //increase so always greater than highest value (always assumed to be the last entry) TODO add max()
-        else
-            if (typeof (_maxkeyvalue) == 'string') {
-                _maxkeyvalue = "X" + _maxkeyvalue;
-            } //increase so always greater than highest value (always assumed to be the last entry) ignoring case sensitivity
-
+        });
 
         //each set is an array of NDTF objects
         //may have been already reformatted and renamed 
 
-        //store the initial key set details in the _output object
+        //store the initial key set details in the _output object in case we need them
 
         this.mergeoutput(_keyset);
 
         //do until the key set records have all been processed
 
-        while (_mergesetindexes[_keyset] < _mergesets[_setid].length) {
+        while (_mergesetindexes[_keyset] < _mergesets[_keyset].length) {
 
             //if we are less than all keys then write out the data and read the next key record
             //if we match any key then store the data and read the next none key record
             //if we are greater than any key then read the next none key record
 
-            for (var _setid in _storedsetids) {
+            _storedsetids.forEach(function (_setid) {
 
-                if (_setid != _keyset) {
+                if (_setid != _keyset) { //ignore the keyset as it will be matched against all the other sets
 
-                    if (_mergekeyvalues[_setid] < _mergekeyvalues[_keyset]) {
-                        _mergesetindexes[_setid]++;
-                    }
-                    else if (_mergekeyvalues[_setid] = _mergekeyvalues[_keyset]) {
-                        //merge this keys data into the output
-                        this.mergeoutput(_setid);
-                        //increment the index for this key
-                        _mergesetindexes[_setid]++;
+                    //if a < b = -1, = = 0 > = 1
 
-                        if (_mergesetindexes[_setid] < _mergesets[_setid].length) {
-                            _mergekeyvalues[_setid] = _mergesets[_setid][_mergesetindexes[_setid]]
+                    //only match if the setid hasnt gone past end of array
+
+                    if (_mergesetindexes[_setid] < _mergesets[_setid].length) {
+
+                        switch (self.comparekeys(_mergesets[_keyset][_mergesetindexes[_keyset]], _mergesets[_setid][_mergesetindexes[_setid]])) {
+                            case -1:
+                                _mergesetindexes[_keyset]++ 
+                                _output = {};
+                                self.mergeoutput(_keyset);
+                                //console.log("<")
+                                break;
+                            case 1:
+                                _mergesetindexes[_setid]++;
+                                //console.log(">")
+                                break;
+                            default: // 0
+                                self.mergeoutput(_setid);
+                                _mergesetindexes[_setid]++;
+                                _processedsetcount++; 
+                                //console.log("=")
+                                
                         }
-                        else { //set this key value to be greater than the highest value in the keyset
-                            _mergekeyvalues[_setid] = _maxkeyvalue;
-                        }
                     }
-
-                    if (_mergekeyvalues[_setid] > _mergekeyvalues[_keyset]) {_processedsetcount++ }
+                    else { //as we dont process this set as it has got the end we set it to processed
+                        _processedsetcount = 99999; //if we hit a situation where any set ends before the key then there cant be any more matches 
+                    }
 
                 }
 
-            }
+            });
 
             //have we processed all the data ?
             //merge the keyset data into the output, and move to the next key value
 
-            if (_processedsetcount > _storedsetids.length - 2 ) { //-2 as we ignore the keyset and need to take one from length
+            if (_processedsetcount > _storedsetids.length - 2) { //-2 as we ignore the keyset and need to take one from length
 
-                //merge key data
+                //we have a valid output for this key so push it and reset unless we hit end of data on one of the sets
+                //first check to see if need to creat it before push
 
-                //store into the new output set
-                _mergedsets.push();
+                if (_processedsetcount < 99999) {
 
-                //read the next one
+                    if (_mergedsets[Object.keys(_output)[0]] == null) {
+                        _mergedsets[Object.keys(_output)[0]] = []
+                    }
+                    
+                    _mergedsets[Object.keys(_output)[0]].push(_output[Object.keys(_output)[0]])
+                    
+                }
+
+                //read the next one and store it in output if we haven't hit the end of data
+                _output = {};
                 _mergesetindexes[_keyset]++
 
+                if (_mergesetindexes[_keyset] < _mergesets[_keyset].length) { this.mergeoutput(_keyset); }
 
                 //reset the processed count to 0
                 _processedsetcount = 0;
             }
 
-        } 
+        }
+
+        //TODO clean out all the saved data quick 
 
         return _mergedsets;
 
@@ -449,8 +475,10 @@ exports.mergeutils = function () {
 
         //here we merge the data in the _output object with the relevant data from the setid 
 
-        if (setid == _keyset) { //this is the key set so we store the key which will hold the array of values
-            _output['setid'] = {};
+        if (setid == _keyset) { //this is the key set so we store the key which will hold the array of values if it isn't present
+            if (_output[_mergesets[setid][_mergesetindexes[setid]][_matchkeys[0]]]==null) {
+                _output[_mergesets[setid][_mergesetindexes[setid]][_matchkeys[0]]] = {}; //should return the 1st key of the set value
+            }
         }
 
         //now store the other fields we have requested
@@ -458,88 +486,112 @@ exports.mergeutils = function () {
         //or null and just whatever hasn't been copied so far 
         //the fields are entries in multiple arrays, keyed on any subsequent match keys
 
-
-
-
+        for (var keyname in _mergesets[setid][_mergesetindexes[setid]]) {
+            if (keyname != _matchkeys[0]) { //ignore the first entry as that is the setid keyname
+                _output[_mergesets[setid][_mergesetindexes[setid]][_matchkeys[0]]][keyname] = _mergesets[setid][_mergesetindexes[setid]][keyname];
+            }
+        }
 
     }
 
-    multi key sort 
+    //multi key sort - each set is sorted by the keys in the config, the keys must be present in all sets
+    //at the end of the sort, all sets including the key set are sorted into multikey order, i.e.a within b within c etc
+    //each level has to be assessed for type primarily because string representation of numbers wont probably work !!
+    //_matchkeys is array of keyname,etc (setid was removed in init)
 
-    homes.sort(
-        function (a, b) {
-            if (a.city === b.city) {
-                // Price is only important when cities are the same
-                return b.price - a.price;
-            }
-            return a.city > b.city ? 1 : -1;
-        });
+    this.sortset = function (set) {
 
-    this.sortset = function (sortkey, set) {
-
-        //determine key data type
-
-        if (typeof (a[sortkey]) == 'number')
-
-            //numeric sort
-            {
-                set.sort(function (a, b) {
-                    return a[sortkey] - b[sortkey];
-                });
-            }
-        
-        else {
-            //string sort - case insensitive
-            if (_casesensitive) {
-                set.sort(function (a, b) {
-                    var x = a[sortkey];
-                    var y = b[sortkey];
-                    if (x < y) { return -1; }
-                    if (x > y) { return 1; }
-                    return 0;
-                });
-            }
-
-            //string sort - case sensitive
-            else {
-                set.sort(function (a, b) {
-                    var x = a[sortkey].toLowerCase();
-                    var y = b[sortkey].toLowerCase();
-                    if (x < y) { return -1; }
-                    if (x > y) { return 1; }
-                    return 0;
-                });
-            }
-        }
+        set.sort(this.comparekeys);
 
         return set;
 
     };
 
-    this.storeset = function (setid, set, sortkey = 'subject') {
+    this.comparekeys = function (a, b) {
+
+        const sortkeys = _sortkeys;
+
+        //based on the sortkeys, we return -1,0,1 
+        //            if (a < b) { return -1; }
+        //            if (a > b) { return 1; }
+        //            return 0;
+
+        // from left to right 
+
+        // if = then check next level, else sort this level
+
+        // for each key, if not equal sort and return, if equal and last key return 0
+
+        for (var idx = 0; idx < sortkeys.length; idx++) {
+
+            var key = sortkeys[idx];
+            var keyvalue = { a: a[_matchkeys[idx]], b: b[_matchkeys[idx]] };
+            var lastkey = (idx == sortkeys.length - 1);
+
+            if (key.keytype == 'n') {
+                if (keyvalue.a < keyvalue.b) { return -1; }
+                if (keyvalue.a > keyvalue.b) { return 1; }
+            }
+
+            if (key.keytype == 's') {
+                if (key.casesensitive) {
+                    if (keyvalue.a.toLowerCase() < keyvalue.b.toLowerCase()) { return -1; }
+                    if (keyvalue.a.toLowerCase() > keyvalue.b.toLowerCase()) { return 1; }
+                }
+                else {
+                    if (keyvalue.a < keyvalue.b) { return -1; }
+                    if (keyvalue.a > keyvalue.b) { return 1; }
+                }
+            }
+
+            if (lastkey) { return 0;}
+
+        }
+
+    };
+
+    this.storeset = function (setid, set) {
+
+        if (_keyset == null) { //first stored set is the key set if not already set in init// 
+            _keyset = setid;
+        }
+
+        //build the sort based on the first entry in the key data set
+
+        if (_keyset == setid) {
+
+            _matchkeys.forEach(function (key) {
+
+                if (typeof (set[0][key]) == 'number') { _sortkeys.push({ keytype: 'n' }); } //if a number
+                else if (typeof (set[0][key]) == 'string') {//if a string, check sensitivity
+                    _sortkeys.push({ keytype: 's', casesensitive: _casesensitive });
+                }
+                else { console.error("sort key requested on value that is not string or numeric"); }
+            });
+
+        }
 
         //the timestamp value must be passed in numeric format (the provider should have already done that)
 
-        var result = _fs.writeFileSync(_path + setid + '.json', JSON.stringify((_presort) ? this.sortset(sortkey, set) : set));
+        var result = _fs.writeFileSync(_path + setid + '.json', JSON.stringify((_presort) ? this.sortset(set) : set));
 
         //if we haven't stored a set add it to list and the sort/merge key
 
         if (_storedsetids.indexOf(setid) == -1) {
             _storedsetids.push(setid);
-            _setkeys[setid] = sortkey;
         }
 
-        if (_keyset == null) { _keyset = setid;} //first stored set is the key set // 
+        
 
         return result;
 
     }
 
-    this.recallset = function (setid,deleteonread=true) {
+    this.recallset = function (setid,deleteonread=false) {
 
         var recalledset = JSON.parse(_fs.readFileSync(_path + setid + '.json', 'utf8')); 
 
-        if (deleteonread) { //clean out the memory as we just loaded another copy !!
+        if (deleteonread) { //clean out the memory
             _fs.unlinkSync(_path + setid + '.json');
         }
 
